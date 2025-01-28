af96d63ce0581af6459e793e647150cc75d85684
basinho <duartebasile6@gmail.com>
2025-01-28 16:33:05 +0100

    delete some fichiers and added the navbar debut

--- actu_f1_app/src/App.vue
+++ actu_f1_app/src/App.vue
@@ -1,19 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import navigationBar from './components/navBar.vue'
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+   
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
-      <nav>
+      
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-      </nav>
+      
+        <navigationBar/>
     </div>
   </header>
 
@@ -21,10 +21,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
+
 header {
   line-height: 1.5;
   max-height: 100vh;
 }
+
+
 
 .logo {
   display: block;
---
+++ actu_f1_app/src/components/navBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav>
+
+    </nav>
+</template>
+
+<script></script>
+
+<style>
+nav{
+    color: red;
+    top: 100%;
+}
+</style>
