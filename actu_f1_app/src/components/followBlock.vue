428f45ec149b0265ba0631f819699897f109fa00
basinho <duartebasile6@gmail.com>
2025-02-10 22:38:37 +0100

    added teams to the follow page

--- actu_f1_app/src/components/followBlock.vue
+++ actu_f1_app/src/components/followBlock.vue
@@ -1,44 +1,34 @@
 <template>
-    <div class="searchBar-div">
-        <input class="searchBar" type="search" placeholder="Search drivers/teams">
-    </div>
-    <div class="circleDiv">
-        <img v-if="img" :src="img" :alt="firstname" />
-    </div>
+        <div class="circleDiv">
+            <img v-if="img" :src="img" :alt="firstname" />
+        </div>
 </template>
 
 <script setup>
     const props = defineProps ({
+        firstname: String,
         img : String,
         img_team : String,
     })
 
 </script>
 <style scoped>
-.searchBar-div{
+
+.circleDiv {
     display: flex;
     justify-content: center;
-}
-.searchBar {
-    width: 75%;
-    height: 5vh;
-  background-color: rgba(255,255,255,0.3);      
-  padding: 10px;
-  border-radius: 25px;
-  color: white;
-}
-
-.searchBar::placeholder {
-  color: #ffffff;  
-  font-style: italic;
-}
-
-.circleDiv{
-    margin-top: 2vh;
-    background-color: rgba(255,255,255,0.7);
+    align-items: center;
     width: 25vw;
-    height: 11.5vh;
-    border-radius: 50px;
-    z-index: 1;
+    height: 12vh;
+    border-radius: 50%;  
+    background-color: rgba(255, 255, 255, 0.7);
+    overflow: hidden; 
+    margin: 0 auto;
+}
+
+img {
+    width: 100%; 
+    height: 100%;
+    object-fit: cover; 
 }
 </style>    
--- actu_f1_app/src/views/mainFollow.vue
+++ actu_f1_app/src/views/mainFollow.vue
@@ -1,7 +1,51 @@
 <template>
-    <followBlock/>
+    <div class="searchBar-div">
+        <input class="searchBar" type="search" placeholder="Search drivers/teams">
+    </div>
+    <div class="grid">
+      <template v-if="drivers.length">
+        <followBlock 
+          v-for="driver in drivers" 
+          :firstname="driver.firstname"
+          :img="driver.img"
+          :img_team="driver.img_team"
+        />
+      </template>
+    </div>
 </template>
 
 <script setup>
+    import { ref, onMounted } from 'vue';
     import followBlock from '../components/followBlock.vue'
+    import dataStanding from '../assets/f1.json';
+
+const drivers = ref(dataStanding); 
 </script>
+
+<style scoped>
+.searchBar-div{
+    display: flex;
+    justify-content: center;
+}
+.searchBar {
+    width: 75%;
+    height: 5vh;
+  background-color: rgba(255,255,255,0.3);      
+  padding: 10px;
+  border-radius: 25px;
+  color: white;
+}
+
+.searchBar::placeholder {
+  color: #ffffff;  
+  font-style: italic;
+}
+.grid{
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    row-gap: 2rem;
+    justify-content: center;
+    margin-top: 4vh;
+    margin-bottom: 30%;
+}
+</style>
